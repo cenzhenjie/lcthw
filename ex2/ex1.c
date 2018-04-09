90d03166965e41d7b2eb810e39a82f3a4cccea93
Zhenjie CEN <cen.zhenjie@hotmail.com>
2018-04-09 10:42:09 +0800

    Exercise 2

---
+++ ex2/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+    puts("Dennis Ritchie is a great programmer");
+    puts("He invented the language C");
+    puts("He worked with his colleague Ken Thompson to invent Unix Operating System");
+    puts("He passed away several years ago");
+
+    return 0;
+}
